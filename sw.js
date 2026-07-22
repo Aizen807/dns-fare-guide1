2a92a47029671ced7e8aa895c3e051f0d4ca60e7
Aizen807 <spectrumnebula0@gmail.com>
2026-07-22 19:33:22 +0800

    Update CACHE_NAME from v6 to v7

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "fare-matrix-v6";
+const CACHE_NAME = "fare-matrix-v7";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {

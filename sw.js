1521d992375a6b0d8d7ae6b7226436d0e58599e9
Aizen807 <spectrumnebula0@gmail.com>
2026-07-24 09:17:46 +0800

    Update service worker cache name to fare-matrix-v8

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "fare-matrix-v7";
+const CACHE_NAME = "fare-matrix-v8";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {

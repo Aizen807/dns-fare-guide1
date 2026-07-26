e633e20d9044d74f95abaf0df985b4bb2101e24f
Aizen807 <spectrumnebula0@gmail.com>
2026-07-27 07:47:49 +0800

    Update CACHE_NAME from fare-matrix-v14 to fare-matrix-v15

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "fare-matrix-v14";
+const CACHE_NAME = "fare-matrix-v15";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
